fc10c5cc56676d58b5aec079088fb7d5e6020f41
Alice Cazati <alice.cazati@gmail.com>
2024-11-13 18:00:19 -0300

    Com alocação de memoria

--- main.c
+++ main.c
@@ -1,95 +1,78 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/*Usando matrizes em funções e procedimentos
-a) Escreva um programa principal em C que crie e armazene uma matriz quadrada de
-tamanho n × n, onde n deve ser lido do teclado e fornecido pelo usuário.
-b) Chame uma função preencheMatriz que preencha a matriz criada no programa prin-
-cipal com valores aleatórios entre 1 e 100.
-c) Chame um procedimento somaDiagonalPrincipal que calcule e exiba a soma dos
-elementos da diagonal principal da matriz. Esse procedimento deve receber uma cópia
-da matriz. Note que essa abordagem não é eficiente em termos de memória, mas será
-utilizada para fins didáticos.
-d) Chame um procedimento somaAbaixoDiagonal que calcule e exiba a soma dos elemen-
-tos abaixo da diagonal principal da matriz. Esse procedimento deve receber o ponteiro
-que aponta para a matriz e o tamanho da matriz, tornando-o mais eficiente, pois
-economiza memória ao evitar a criação de uma cópia desnecessária da matriz.
-e) Implemente as funções e procedimentos utilizados no programa principal:
-• preencheMatriz
-• somaDiagonalPrincipal
-• somaAbaixoDiagonal
-2
-*/
-void preencherMatriz(int n,int M[n][n])
-{
-    for ( int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            M[i][j]=rand()%100+1;
+
+void preencherMatriz(int n, int* M) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            *(M+i*n+j) = rand() % 100 + 1;
         }
-        
     }
-    
-}
-void somaDiagonalPrincipal(int n,int M[n][n])
-{
-    int soma=0;
-   for(int i=0;i<n;i++)
-   {
-     for(int j=0;j<n;j++)
-     {
-        if(i==j)
-        {
-            soma+=M[i][j];
-        }
-     }
-   }
-   printf("A soma da diagonal principal:%i\n",soma);
 }
 
-void somaAbaixoDiagonal(int n, int (*M)[n])
-{
-    int soma=0;
-    for(int i=0;i<n;i++)
-   {
+void somaDiagonalPrincipal(int n, int* M) {
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
      for(int j=0;j<n;j++)
      {
-        if(i>j)
+        if (i==j)
         {
-            soma+=M[i][j];
+           soma += *(M+i*n+j);
         }
+        
+        
      }
-   }
-   printf("A soma da abaixo da diagonal principal :%i",soma);
+        
+    }
+    printf("A soma da diagonal principal: %i\n", soma);
 }
-void somaDiagonalEsquerda(int n,int (*M)[n])
-{
-    int soma=0;
-    for(int i=0;i<n;i++)
-   {
-     soma+=M[i][n-1-i];
-   }
-   printf("\nSoma da diagonal da esquerda:%i",soma);
+
+void somaAbaixoDiagonal(int n, int *M) {
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            soma += *(M+i*n+j);
+        }
+    }
+    printf("A soma abaixo da diagonal principal: %i\n", soma);
 }
-int main()
-{
+
+void somaDiagonalEsquerda(int n, int *M) {
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma += *(M + i * n + (n - 1 - i));
+    }
+    printf("Soma da diagonal da esquerda: %i\n", soma);
+}
+
+int main() {
     srand(time(NULL));
-   int n;
-   printf("Digite o valor de n:");
-   scanf("%i",&n);
-   int M[n][n];
-   preencherMatriz(n,M);
-   for (int i = 0; i < n; i++)
-   {
-     for (int j = 0; j < n; j++)
-     {
-        printf("%i\t",M[i][j]);
-     }
-     printf("\n");
-   }
-   somaDiagonalPrincipal(n,M);
-   somaAbaixoDiagonal(n,M);
-   somaDiagonalEsquerda(n,M);
-   return 0;
+    int n;
+
+    printf("Digite o valor de n: ");
+    scanf("%i", &n);
+
+    int *M=(int*)malloc(n*n*sizeof(int));
+    if (M==NULL)
+    {
+        printf("Erro");
+        return 1;
+    }
+    
+    preencherMatriz(n, M);
+
+    printf("Matriz gerada:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3i\t", *(M+i*n+j) );
+        }
+        printf("\n");
+    }
+
+    somaDiagonalPrincipal(n, M);
+    somaAbaixoDiagonal(n, M);
+    somaDiagonalEsquerda(n, M);
+    free(M);
+
+    return 0;
 }
